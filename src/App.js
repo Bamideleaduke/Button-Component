89f0d9991ed224182b937fe85d78d491cf82d994
Bamideleaduke <bamideleaduke@gmail.com>
2022-12-28 12:58:15 +0100

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,70 @@
 import "./App.css";
 import Button from "./components/Button";
-import Aside from "./components/Aside"
+import Aside from "./components/Aside";
 
 const footer = (
-  <footer className="footer">created by bamideleaduke - devChallenges.io</footer>
-  );
-
+  <footer className="footer">
+    created by bamideleaduke - devChallenges.io
+  </footer>
+);
 
 function App() {
   return (
     <div className="App body">
-      <Aside/>
-      
-    <main className="main">
-    <h2 className="title">Buttons</h2>
+      <Aside />
 
-    {/* default */}
-   <section >
-    <h5>Default styling with :hover</h5>
-    <div className="btn">
-    <Button text="Default" />
-    <Button color="outline" text="Default" />
-    <Button color="text" text="Default" />
-    <Button color="disableShadow primary" text="Default" />
-    </div>
-   </section>
+      <main className="main">
+        <h2 className="title">Buttons</h2>
 
-   {/* disabled */}
-   <section >
-   <h5>Disabled</h5>
-  <div className="btn s-around">
-    <Button color="disabled default" text="Default" />
-    <Button color="text disabled" text="Default" />
-  </div>
-   </section>
+        {/* default */}
+        <section>
+          <h5>Default styling with :hover</h5>
+          <div className="btn">
+            <Button text="Default" />
+            <Button color="outline" text="Default" />
+            <Button color="text" text="Default" />
+            <Button color="disableShadow primary" text="Default" />
+          </div>
+        </section>
 
-     {/* with icons */}
-   <section >
-     <h5>start icon and end icon</h5>
-   <div className="btn s-around">
-      <Button starticon color="icon primary" text="Default" />
-      <Button endicon color="icon primary" text="Default" />
-   </div>
-   </section>
+        {/* disabled */}
+        <section>
+          <h5>Disabled</h5>
+          <div className="btn s-around">
+            <Button color="disabled default" text="Default" />
+            <Button color="text disabled" text="Default" />
+          </div>
+        </section>
 
-      {/* sizes */}
-    <section>
-      <h5>Default sizes</h5>
-    <div className="btn">
-      <Button color="disableShadow sm primary" text="Default" />
-     <Button color="disableShadow md primary" text="Default" />
-     <Button color="disableShadow lg primary" text="Default" />
-    </div>
-    </section>
+        {/* with icons */}
+        <section>
+          <h5>start icon and end icon</h5>
+          <div className="btn s-around">
+            <Button starticon color="icon primary" text="Default" />
+            <Button endicon color="icon primary" text="Default" />
+          </div>
+        </section>
 
-      {/* colors */}
-     <section>
-      <h5>Default colors</h5>
-    <div className="btn">
-      <Button color="sm default" text="Default" />
-      <Button color="md primary" text="Default" />
-      <Button color="lg secondary" text="Default" />
-    </div>
-     </section>
-      {footer}
+        {/* sizes */}
+        <section>
+          <h5>Default sizes</h5>
+          <div className="btn">
+            <Button color="disableShadow sm primary" text="Default" />
+            <Button color="disableShadow md primary" text="Default" />
+            <Button color="disableShadow lg primary" text="Default" />
+          </div>
+        </section>
+
+        {/* colors */}
+        <section>
+          <h5>Default colors</h5>
+          <div className="btn">
+            <Button color="sm default" text="Default" />
+            <Button color="md primary" text="Default" />
+            <Button color="lg secondary" text="Default" />
+          </div>
+        </section>
+        {footer}
       </main>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-import "../App.css"
+import "../App.css";
 
-
-
-const Button = ({color,text,starticon,endicon}) =>{
-    return <button className={`${color}`}>
-        {starticon && <span className="starticon"><i className="fa-solid fa-cart-shopping"></i></span> }
-    {text}
-        {endicon && <span className="endicon"><i className="fa-solid fa-cart-shopping"></i></span> }
-        </button>
-}
+const Button = ({ color, text, starticon, endicon }) => {
+  return (
+    <button className={`${color}`}>
+      {starticon && (
+        <span className="starticon">
+          <i className="fa-solid fa-cart-shopping"></i>
+        </span>
+      )}
+      {text}
+      {endicon && (
+        <span className="endicon">
+          <i className="fa-solid fa-cart-shopping"></i>
+        </span>
+      )}
+    </button>
+  );
+};
 
 export default Button;
